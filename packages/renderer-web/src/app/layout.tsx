b60b033ea455fd1e88d889e64e744102d1839f5e
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2024-01-14 11:18:54 +0100

    chore: bump all deps to latest

--- packages/renderer-web/src/app/layout.tsx
+++ packages/renderer-web/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Viewport } from 'next';
 import type React from 'react';
 
 import { GlobalStyles, ResetStyles } from '#pkg/app/global-styles';
@@ -29,6 +29,7 @@ export default function RootLayout({ children }: LayoutProps) {
   );
 }
 
-export const metadata: Metadata = {
-  viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
 };
