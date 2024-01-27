cc7f2aba099e75d21208f7f18de9ab72e86c5ab9
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2024-01-27 16:32:16 +0100

    chore: improve linting rules

--- packages/renderer-web/.eslintrc.cjs
+++ packages/renderer-web/.eslintrc.cjs
@@ -1,18 +1,27 @@
 // @ts-check
 const baseEslintConfig = require('@document-generator/config-eslint/eslint-ecma.cjs');
+const reactEslintConfig = require('@document-generator/config-eslint/eslint-react.cjs');
 const nextEslintConfig = require('@document-generator/config-eslint/eslint-next.cjs');
 
 module.exports = {
   ...baseEslintConfig,
+  ...reactEslintConfig,
   ...nextEslintConfig,
   parserOptions: {
     ...baseEslintConfig.parserOptions,
+    ...reactEslintConfig.parserOptions,
     ...nextEslintConfig.parserOptions,
     tsconfigRootDir: __dirname,
   },
-  plugins: [...(baseEslintConfig.plugins ?? []), ...(nextEslintConfig.plugins ?? []), 'jsx-a11y'],
+  plugins: [
+    ...(baseEslintConfig.plugins ?? []),
+    ...(reactEslintConfig.plugins ?? []),
+    ...(nextEslintConfig.plugins ?? []),
+    'jsx-a11y',
+  ],
   extends: [
     ...baseEslintConfig.extends,
+    ...reactEslintConfig.extends,
     ...nextEslintConfig.extends,
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
@@ -20,17 +29,20 @@ module.exports = {
   ],
   ignorePatterns: [
     ...(baseEslintConfig.ignorePatterns ?? []),
+    ...(reactEslintConfig.ignorePatterns ?? []),
     ...(nextEslintConfig.ignorePatterns ?? []),
     'next.config.js',
   ],
   rules: {
     ...baseEslintConfig.rules,
+    ...reactEslintConfig.rules,
     ...nextEslintConfig.rules,
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
   },
   overrides: [
     ...(baseEslintConfig.overrides ?? []),
+    ...(reactEslintConfig.overrides ?? []),
     ...(nextEslintConfig.overrides ?? []),
     {
       // allow default export for Next.js pages
